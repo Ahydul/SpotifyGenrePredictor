2540391a5a2803292156b01f177efda880fc5b1c
mromalde <mromalde@gmail.com>
2023-05-04 15:08:42 +0200

    Added create file json with the requirements features

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,7 +1,8 @@
+
 var express = require('express');
 var router = express.Router();
 var spotifyApi = require('../controllers/spotify.js');
-
+const fs = require('fs')
 router.post('/tracks',async function(req, res, next) {
   var playlistId = req.body.playlist;
 // Get a user's playlists
@@ -16,6 +17,7 @@ var aux = await spotifyApi.getPlaylist(playlistId, {
 });
 
 // Get playlist's tracks
+var detailedPlaylists=[];
 var detailedPlaylist = aux.body.tracks.items;
 
   var playlistsTrackIds = await detailedPlaylist
@@ -25,12 +27,53 @@ var detailedPlaylist = aux.body.tracks.items;
   const audioFeatures = await spotifyApi.getAudioFeaturesForTracks(playlistsTrackIds);
   // Get track's audio features
 
-  detailedPlaylist.push(audioFeatures);
+  detailedPlaylists.push(audioFeatures);
+  var songFeatures=[]
+
+  for (let i = 0; i < playlist.body.tracks.items.length; i++) {
+    track_id=playlist.body.tracks.items[i].track.id;
+    duration_ms=playlist.body.tracks.items[i].track.duration_ms;
+    danceability = detailedPlaylists[0].body.audio_features[i].danceability;
+    energy = detailedPlaylists[0].body.audio_features[i].energy;
+    key = detailedPlaylists[0].body.audio_features[i].key;
+    loudness = detailedPlaylists[0].body.audio_features[i].loudness;
+    mode = detailedPlaylists[0].body.audio_features[i].mode;
+    speechiness = detailedPlaylists[0].body.audio_features[i].speechiness;
+    acousticness = detailedPlaylists[0].body.audio_features[i].acousticness;
+    instrumentalness = detailedPlaylists[0].body.audio_features[i].instrumentalness;
+    liveness = detailedPlaylists[0].body.audio_features[i].liveness;
+    valence = detailedPlaylists[0].body.audio_features[i].valence;
+    tempo = detailedPlaylists[0].body.audio_features[i].tempo;
+    time_signature = detailedPlaylists[0].body.audio_features[i].time_signature;
+  
+    const song = {
+      track_id: track_id,
+      duration_ms: duration_ms,
+      danceability: danceability,
+      energy: energy,
+      key: key,
+      loudness: loudness,
+      mode: mode,
+      speechiness: speechiness,
+      acousticness: acousticness,
+      instrumentalness: instrumentalness,
+      liveness: liveness,
+      valence: valence,
+      tempo: tempo,
+      time_signature: time_signature
+    };
+    songFeatures.push(song);
+  }
+  //res.json(songFeatures);
+  fs.writeFileSync('tracksFeatures.json', JSON.stringify(songFeatures));
 
   res.render('tracks', { title: 'tracks', 
   playlistId: playlistId,
   songs:playlist.body.tracks.items,
-  features:detailedPlaylist
+  features:detailedPlaylist,
+  track_id:track_id,
+  duration_ms:duration_ms,
+  danceability:danceability
 });
 
 });
